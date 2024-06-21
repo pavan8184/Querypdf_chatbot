ad9424e64512b9ec3a14d867ecf5ee9d27bba3df
prudhvipavan.s <109591373+pavan8184@users.noreply.github.com>
2024-06-21 18:09:21 +0530

    Update extract_text.py

--- extract_text.py
+++ extract_text.py
@@ -1,17 +1,10 @@
 import PyPDF2
  
 def extract_text_from_pdf(pdf_path):
-    try:
-        with open(pdf_path, 'rb') as file:
-            reader = PyPDF2.PdfFileReader(file)
-            text = ''
-            for page_num in range(reader.numPages):
-                page = reader.getPage(page_num)
-                text += page.extract_text()
-            return text
-    except PyPDF2.utils.PdfReadError as e:
-        print(f"Error reading PDF file: {e}")
-        return None
-    except FileNotFoundError:
-        print(f"PDF file not found at: {pdf_path}")
-        return None
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfFileReader(file)
+        text = ''
+        for page_num in range(reader.numPages):
+            page = reader.getPage(page_num)
+            text += page.extract_text()
+        return text
