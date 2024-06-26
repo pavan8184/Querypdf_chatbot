5fda42fb1fbcb3e96ae3d859cb630aaa0f160449
pavan8184 <pavansakhumalla@gmail.com>
2024-06-26 15:12:55 +0530

    changed

--- extract_text.py
+++ extract_text.py
@@ -4,7 +4,8 @@ def extract_text_from_pdf(pdf_path):
     text = ""
     with open(pdf_path, 'rb') as file:
         reader = PyPDF2.PdfFileReader(file)
-        for page_num in range(reader.numPages):
+        num_pages = reader.numPages
+        for page_num in range(num_pages):
             page = reader.getPage(page_num)
-            text += page.extract_text()
+            text += page.extractText()
     return text
