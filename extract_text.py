a2a935860e3c469f6af9e617cd8d95059f3813a9
pavan8184 <pavansakhumalla@gmail.com>
2024-06-26 14:59:34 +0530

    replaced

--- extract_text.py
+++ extract_text.py
@@ -1,9 +1,10 @@
-import fitz  # PyMuPDF
+import PyPDF2
  
 def extract_text_from_pdf(pdf_path):
     text = ""
-    document = fitz.open(pdf_path)
-    for page_num in range(len(document)):
-        page = document.load_page(page_num)
-        text += page.get_text()
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfFileReader(file)
+        for page_num in range(reader.numPages):
+            page = reader.getPage(page_num)
+            text += page.extract_text()
     return text
